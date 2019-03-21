1c90140349ebee4bfae77cc4993657d3baced735
waltersgit <designs.peter@gmail.com>
2019-03-21 12:54:32 +0800

    test

---
+++ frontend/build/precache-manifest.0069ba5af99fc2c0c518379f8fed923f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a996b3b06cc2407088a",
+    "url": "/static/css/main.bf1d313f.chunk.css"
+  },
+  {
+    "revision": "8a996b3b06cc2407088a",
+    "url": "/static/js/main.8a996b3b.chunk.js"
+  },
+  {
+    "revision": "e7bbbdaf7298e95234f8",
+    "url": "/static/css/1.4920accb.chunk.css"
+  },
+  {
+    "revision": "e7bbbdaf7298e95234f8",
+    "url": "/static/js/1.e7bbbdaf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "db2a924730de94010805d80ef80c1c61",
+    "url": "/index.html"
+  }
+];
